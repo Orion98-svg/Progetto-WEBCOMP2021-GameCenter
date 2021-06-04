d9118f89abdbfa9cdde16f17ca028ddd23ace22d
Serrao-nyes <serrao.giuseppe48@gmail.com>
2021-06-04 15:16:33 +0200

    Funziona il fetchGames di prova

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -5,3 +5,35 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active')
     menulinks.classList.toggle('active')
 })
+
+async function fetchGames(page){
+    $.ajax({
+        type:'GET',
+        url:"https://api.rawg.io/api/games?key=2d150e2f5c964e6992d048af8ef065f7&page="+page+"&page_size=10",
+        success:function (result){
+            for(var i=0;i<result.results.length;i++)
+            {
+                creatGame(result.results[i].name,result.results[i].background_image);
+            }
+        }
+
+    });
+}
+
+function creatGame(name,image) {
+    var Space = document.createElement('div');
+    Space.classList.add("GameSpace");
+    Space.className = "GameSpace"
+    var Img = document.createElement('div')
+    Img.classList.add("GameImage")
+    Img.style.backgroundImage = "url(" + image + ")"
+    var Name = document.createElement('div');
+    Name.classList.add("GameName");
+    Name.textContent = name
+    Space.append(Img);
+    Space.append(Name);
+    var element = document.getElementById("games");
+    element.append(Space);
+}
+
+fetchGames(1);
