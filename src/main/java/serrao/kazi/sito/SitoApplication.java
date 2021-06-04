a87b4015c5ef10b8598d252c1401b0b728ff1998
Orion <shahnewazkazi7@gmail.com>
2021-06-04 14:50:41 +0200

    new site

---
+++ src/main/java/serrao/kazi/sito/SitoApplication.java
@@ -0,0 +1,13 @@
+package serrao.kazi.sito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitoApplication.class, args);
+	}
+
+}
