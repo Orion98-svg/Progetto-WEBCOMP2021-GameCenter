539d46dc257246ad06971a13ba619b698b0dadbf
Serrao-nyes <serrao.giuseppe48@gmail.com>
2021-06-08 16:14:51 +0200

    Aggiunte funzioni per il filtro degli store rudimentale

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -8,13 +8,17 @@ menu.addEventListener('click', function(){
 const Posizione = document.getElementById("Page_btn")
 var Button_Render = 0;
 const GameContainer= document.getElementById('games')
+const PlatformContainer=document.getElementById("Platforms")
 var PageNumber=1
 var PageSize=40
-document.onload=fetchGames(PageNumber,PageSize)
+var Store
+var Filtred=false
+document.onload=fetchGames(PageNumber,PageSize,Store),createStoreBadge()
 
 
-async function fetchGames(page,size){
-    $.ajax({
+async function fetchGames(page,size,store){
+    if(!Filtred)
+    { $.ajax({
         type:'GET',
         url:"https://api.rawg.io/api/games?key=2d150e2f5c964e6992d048af8ef065f7&page="+page+"&page_size="+size,
         success:function (result){
@@ -24,7 +28,106 @@ async function fetchGames(page,size){
             }
             createButton();
         }
-    });
+    });}
+    else
+    {
+        $.ajax({
+            type:'GET',
+            url:"https://api.rawg.io/api/games?key=2d150e2f5c964e6992d048af8ef065f7&page="+page+"&page_size="+size+"&stores="+store,
+            success:function (result){
+                for(var i=0;i<result.results.length;i++)
+                {
+                    creatGame(result.results[i].name,result.results[i].background_image);
+                }
+                createButton();
+            }
+        });
+    }
+}
+
+function createStoreBadge()
+{
+    var steam=document.createElement("button")
+    steam.classList.add("badge")
+    steam.setAttribute("id","steam")
+    PlatformContainer.append(steam)
+    steam.setAttribute("style",  "background-image: url('../images/steam-logo-transparent.png')")
+    steam.addEventListener('click',function (){
+
+        Filtred=true
+        Store=1
+        cleanGameContainer()
+        cleanPosizione()
+        fetchGames(PageNumber,PageSize,Store)
+    })
+    var epic=document.createElement("button")
+    epic.classList.add("badge")
+    epic.setAttribute("id","epic")
+    PlatformContainer.append(epic)
+    epic.setAttribute("style",  "background-image: url('../images/epic.png')")
+    epic.addEventListener('click',function (){
+
+        Filtred=true
+        Store=11
+        cleanGameContainer()
+        cleanPosizione()
+        fetchGames(PageNumber,PageSize,Store)
+    })
+    var xbox=document.createElement("button")
+    xbox.classList.add("badge")
+    xbox.setAttribute("id","xbox")
+    PlatformContainer.append(xbox)
+    xbox.setAttribute("style",  "background-image: url('../images/xbox.png')")
+    xbox.addEventListener('click',function (){
+        Filtred=true
+        Store=7
+        cleanGameContainer()
+        cleanPosizione()
+        fetchGames(PageNumber,PageSize,Store)
+    })
+    var gog=document.createElement("button")
+    gog.classList.add("badge")
+    gog.setAttribute("id","gog")
+    PlatformContainer.append(gog)
+    gog.setAttribute("style",  "background-image: url('../images/gog.png')")
+    gog.addEventListener('click',function (){
+        Filtred=true
+        Store=5
+        cleanGameContainer()
+        cleanPosizione()
+        fetchGames(PageNumber,PageSize,Store)
+    })
+    var playstation=document.createElement("button")
+    playstation.classList.add("badge")
+    playstation.setAttribute("id","playstation")
+    PlatformContainer.append(playstation)
+    playstation.setAttribute("style",  "background-image: url('../images/playstation.jpg')")
+    playstation.addEventListener('click',function (){
+        Filtred=true
+        Store=3
+        cleanGameContainer()
+        cleanPosizione()
+        fetchGames(PageNumber,PageSize,Store)
+    })
+    var nintendo=document.createElement("button")
+    nintendo.classList.add("badge")
+    nintendo.setAttribute("id","nintendo")
+    PlatformContainer.append(nintendo)
+    nintendo.setAttribute("style",  "background-image: url('../images/nintendo.png')")
+    nintendo.addEventListener('click',function (){
+        Filtred=true
+        Store=6
+        cleanGameContainer()
+        cleanPosizione()
+        fetchGames(PageNumber,PageSize,Store)
+    })
+
+
+   /* var microsoftstore
+
+    var playstore
+    var applestore
+    var ithci*/
 }
 
 function creatGame(name,image) {
@@ -85,7 +188,7 @@ function changePageForward()
     cleanGameContainer()
     cleanPosizione()
     PageNumber++
-    fetchGames(PageNumber,PageSize)
+    fetchGames(PageNumber,PageSize,Store)
 }
 function changePageBackward()
 {
@@ -94,6 +197,6 @@ function changePageBackward()
         cleanGameContainer()
         cleanPosizione()
         PageNumber--
-        fetchGames(PageNumber,PageSize)
+        fetchGames(PageNumber,PageSize,Store)
     }
 }
